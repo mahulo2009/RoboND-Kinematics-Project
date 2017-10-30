73d1374b3c2561c6c0251824caac086039dd9c64
Manuel <manuel.huertas.lopez@gmail.com>
2017-10-30 09:21:18 +0000

    forward kinematic

--- kinematics/src/kuka_arm_fk.py
+++ kinematics/src/kuka_arm_fk.py
@@ -22,6 +22,7 @@ s = {
      
      }
 
+# Homogeneous Transforms Matrix
 T0_1 = Matrix([[             cos(q1),            -sin(q1),            0,              a0],
                [ sin(q1)*cos(alpha0), cos(q1)*cos(alpha0), -sin(alpha0), -sin(alpha0)*d1],
                [ sin(q1)*sin(alpha0), cos(q1)*sin(alpha0),  cos(alpha0),  cos(alpha0)*d1],
@@ -64,57 +65,51 @@ T6_7 = Matrix([[             cos(q7),            -sin(q7),            0,        
                [                   0,                   0,            0,               1]])
 T6_7 = T6_7.subs(s)
 
+# Rotation to have the same orientation than rviz
+R_z = Matrix([[ cos(np.pi),     -sin(np.pi),    0,  0],
+               [sin(np.pi),     cos(np.pi),     0,  0],
+               [0,              0,              1,  0],
+               [0,              0,              0,  1]])
 
+R_y = Matrix([[ cos(-np.pi/2),  0,              sin(-np.pi/2),   0],
+              [ 0,              1,              0,               0],
+              [ -sin(-np.pi/2), 0,              cos(-np.pi/2),   0],
+              [ 0,              0,              0,               1]])
+
+R_corr = simplify(R_z * R_y)
+
+
+
+# EE point
 P_0_0 = Matrix([[0],[0],[0],[1]])
 
+# Intermediate transformations
 T0_2 = simplify( T0_1 * T1_2 )
 T0_3 = simplify( T0_2 * T2_3 )
 T0_4 = simplify( T0_3 * T3_4 )
 T0_5 = simplify( T0_4 * T4_5 )
 T0_6 = simplify( T0_5 * T5_6 )
 T0_G = simplify( T0_6 * T6_7 )
-
-#T0_7 = simplify(T0_G * P_0_0 )
-
-R_z = Matrix([[ cos(np.pi),     -sin(np.pi),    0,  0],
-               [sin(np.pi),     cos(np.pi),     0,  0],
-               [0,              0,              0,  1],
-               [0,              0,              0,  1]])
-
-R_y = Matrix([[ cos(-np.pi/2),  0,              sin(-np.pi/2),   0],
-              [ 0,              1,              0,               0],
-              [ -sin(-np.pi/2), 0,              cos(-np.pi/2),   1],
-              [ 0,              0,              0,               1]])
-
-R_corr = simplify(R_z * R_y)
-
-
-q1_in = 0.79
-q2_in = 0.47
-q3_in = -0.14
-q4_in = -0.68
-q5_in = -0.72
-q6_in = -1.96
-q7_in = 0.0
-
-print("T0_1->",(T0_1*P_0_0).evalf(subs={q1: 0, q2:0, q3:0, q4:0, q5:0, q6:0, q7:0}))
-print("T0_2->",(T0_2*P_0_0).evalf(subs={q1: 0, q2:0, q3:0, q4:0, q5:0, q6:0, q7:0}))
-print("T0_3->",(T0_3*P_0_0).evalf(subs={q1: 0, q2:0, q3:0, q4:0, q5:0, q6:0, q7:0}))
-print("T0_4->",(T0_4*P_0_0).evalf(subs={q1: 0, q2:0, q3:0, q4:0, q5:0, q6:0, q7:0}))
-print("T0_5->",(T0_5*P_0_0).evalf(subs={q1: 0, q2:0, q3:0, q4:0, q5:0, q6:0, q7:0}))
-print("T0_6->",(T0_6*P_0_0).evalf(subs={q1: 0, q2:0, q3:0, q4:0, q5:0, q6:0, q7:0}))
-print("T0_G->",(T0_G*P_0_0).evalf(subs={q1: q1_in, q2:q2_in, q3:q3_in, q4:q4_in, q5:q5_in, q6:q6_in, q7:q7_in}))
-
 T_total = simplify(T0_G * R_corr)
 
-print ("T_total->",T_total)
 
 
-#T0_1 = simplify(T0_1*P_0_0)
-#print("T0_1->",T0_1.evalf(subs={q1: 0}))
+q1_in = 0.80
+q2_in = 0.47
+q3_in = -0.14
+q4_in = -0.67
+q5_in = -0.72
+q6_in = -1.97
 
-#T0_2 = simplify(T1_2 * P_0_0)
-#print("T1_2->",T0_2.evalf(subs={q1: 0,q2:0}))
+
+print("T0_1->",(T0_1*P_0_0).evalf(subs={q1: q1_in, q2:q2_in, q3:q3_in, q4:q4_in, q5:q5_in, q6:q6_in}))
+print("T0_2->",(T0_2*P_0_0).evalf(subs={q1: q1_in, q2:q2_in, q3:q3_in, q4:q4_in, q5:q5_in, q6:q6_in}))
+print("T0_3->",(T0_3*P_0_0).evalf(subs={q1: q1_in, q2:q2_in, q3:q3_in, q4:q4_in, q5:q5_in, q6:q6_in}))
+print("T0_4->",(T0_4*P_0_0).evalf(subs={q1: q1_in, q2:q2_in, q3:q3_in, q4:q4_in, q5:q5_in, q6:q6_in}))
+print("T0_5->",(T0_5*P_0_0).evalf(subs={q1: q1_in, q2:q2_in, q3:q3_in, q4:q4_in, q5:q5_in, q6:q6_in}))
+print("T0_6->",(T0_6*P_0_0).evalf(subs={q1: q1_in, q2:q2_in, q3:q3_in, q4:q4_in, q5:q5_in, q6:q6_in}))
+print("T0_G->",(T0_G*P_0_0).evalf(subs={q1: q1_in, q2:q2_in, q3:q3_in, q4:q4_in, q5:q5_in, q6:q6_in}))
+print("T_total->",(T_total * P_0_0).evalf(subs={q1: q1_in, q2:q2_in, q3:q3_in, q4:q4_in, q5:q5_in, q6:q6_in}))
 
 
 
