1c24802d0f94ebe63e81f2e6a773990a0e77395d
Manuel <manuel.huertas.lopez@gmail.com>
2017-10-31 10:12:15 +0000

    IK server simplify

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -69,40 +69,36 @@ def handle_calculate_IK(req):
                        [                   0,                   0,            0,               1]])
         T2_3 = T2_3.subs(s)
         
-        T3_4 = Matrix([[             cos(q4),            -sin(q4),            0,              a3],
-                       [ sin(q4)*cos(alpha3), cos(q4)*cos(alpha3), -sin(alpha3), -sin(alpha3)*d4],
-                       [ sin(q4)*sin(alpha3), cos(q4)*sin(alpha3),  cos(alpha3),  cos(alpha3)*d4],
-                       [                   0,                   0,            0,               1]])
-        T3_4 = T3_4.subs(s)
-        
-        T4_5 = Matrix([[             cos(q5),            -sin(q5),            0,              a4],
-                       [ sin(q5)*cos(alpha4), cos(q5)*cos(alpha4), -sin(alpha4), -sin(alpha4)*d5],
-                       [ sin(q5)*sin(alpha4), cos(q5)*sin(alpha4),  cos(alpha4),  cos(alpha4)*d5],
-                       [                   0,                   0,            0,               1]])
-        T4_5 = T4_5.subs(s)
-        
-        T5_6 = Matrix([[             cos(q6),            -sin(q6),            0,              a5],
-                       [ sin(q6)*cos(alpha5), cos(q6)*cos(alpha5), -sin(alpha5), -sin(alpha5)*d6],
-                       [ sin(q6)*sin(alpha5), cos(q6)*sin(alpha5),  cos(alpha5),  cos(alpha5)*d6],
-                       [                   0,                   0,            0,               1]])
-        T5_6 = T5_6.subs(s)
-        
-        T6_7 = Matrix([[             cos(q7),            -sin(q7),            0,              a6],
-                       [ sin(q7)*cos(alpha6), cos(q7)*cos(alpha6), -sin(alpha6), -sin(alpha6)*d7],
-                       [ sin(q7)*sin(alpha6), cos(q7)*sin(alpha6),  cos(alpha6),  cos(alpha6)*d7],
-                       [                   0,                   0,            0,               1]])
-        T6_7 = T6_7.subs(s)
-    
     # Create individual transformation matrices
     #
     #
-        T0_2 = simplify( T0_1 * T1_2 )
-        T0_3 = simplify( T0_2 * T2_3 )
-        T0_4 = simplify( T0_3 * T3_4 )
-        T0_5 = simplify( T0_4 * T4_5 )
-        T0_6 = simplify( T0_5 * T5_6 )
-        T0_G = simplify( T0_6 * T6_7 )
-        T_total = simplify(T0_G * R_corr)
+        R_z = Matrix([[ cos(np.pi),     -sin(np.pi),    0],
+               [sin(np.pi),     cos(np.pi),     0],
+               [0,              0,              1],
+               ])
+
+        R_y = Matrix([[ cos(-np.pi/2),  0,              sin(-np.pi/2)],
+              [ 0,              1,              0,],
+              [ -sin(-np.pi/2), 0,              cos(-np.pi/2)],
+              ])
+
+        R_corr = simplify(R_z * R_y)
+
+        R_EEx = Matrix([[ 1,           0,   0],
+              [ 0,      cos(alpha0),    -sin(alpha0)],
+              [ 0,      sin(alpha0),    cos(alpha0)]])
+
+        R_EEy = Matrix([[ cos(alpha1),        0,  sin(alpha1)],
+              [       0,        1,        0],
+              [-sin(alpha1),        0,  cos(alpha1)]])
+
+        R_EEz = Matrix([[ cos(alpha2), -sin(alpha2),        0],
+              [ sin(alpha2),  cos(alpha2),        0],
+              [ 0,              0,        1]])
+
+        R_EE_corr = simplify ( R_EEz * R_EEy * R_EEx * R_corr )
+        
+
 
     
     # Extract rotation matrices from the transformation matrices
@@ -111,12 +107,11 @@ def handle_calculate_IK(req):
         R0_1 = T0_1.extract([0,1,2],[0,1,2])
         R1_2 = T1_2.extract([0,1,2],[0,1,2])
         R2_3 = T2_3.extract([0,1,2],[0,1,2])
-        R3_4 = T3_4.extract([0,1,2],[0,1,2])
-        R4_5 = T4_5.extract([0,1,2],[0,1,2])
-        R5_6 = T5_6.extract([0,1,2],[0,1,2])
     
         R_0_3 = simplify(R0_1  * R1_2 * R2_3)
         R_0_3_Inv = R_0_3.inv("LU")
+        
+
         ###
 
         # Initialize service response
@@ -141,19 +136,6 @@ def handle_calculate_IK(req):
         # Compensate for rotation discrepancy between DH parameters and Gazebo
         #
         #
-            R_EEx = Matrix([[ 1,           0,   0],
-                  [ 0,      cos(alpha0),    -sin(alpha0)],
-                  [ 0,      sin(alpha0),    cos(alpha0)]])
-    
-            R_EEy = Matrix([[ cos(alpha1),        0,  sin(alpha1)],
-                  [       0,        1,        0],
-                  [-sin(alpha1),        0,  cos(alpha1)]])
-    
-            R_EEz = Matrix([[ cos(alpha2), -sin(alpha2),        0],
-                  [ sin(alpha2),  cos(alpha2),        0],
-                  [ 0,              0,        1]])
-    
-            R_EE_corr = simplify ( R_EEz * R_EEy * R_EEx * R_corr )
             R_EE_corr_eval =R_EE_corr.evalf(subs={alpha0: roll, alpha1:pitch, alpha2:yaw})
 
         # Calculate joint angles using Geometric IK method
@@ -172,6 +154,7 @@ def handle_calculate_IK(req):
             theta2 = math.pi/2-math.acos(np.clip((B*B+C*C-A*A)/(2*B*C),-1,1))-math.atan2(L,W)
             theta3 = math.pi/2-(b+0.036)
             
+            R_3_6 = simplify(R_0_3_Inv * R_EE_corr_eval)
             R_3_6_eval= R_3_6.evalf(subs={q1: theta1, q2:theta2, q3:theta3})
     
             theta5 = math.acos(R_3_6_eval[1,2])
